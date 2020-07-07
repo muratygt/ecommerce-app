5830fd6a16cac1453778c3a353d7e6e99ef27bd5
Matt <muratygt@icloud.com>
2020-07-07 02:00:36 -0400

    Add Signin, Signup with their styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from './components/homepage/Homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInSignUpPage from './components/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInSignUpPage}/>
       </Switch>
     </div>
   );
---
+++ src/components/sign-in-sign-up/sign-in-sign-up.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './sign-in-sign-up.styles.scss';
+import SignIn from '../sign-in/sign-in.component';
+
+const SignInSignUpPage = () => {
+    return ( 
+        <div className='sign-in-sign-up'>
+            <SignIn/>
+        </div>
+     );
+}
+ 
+export default SignInSignUpPage;
